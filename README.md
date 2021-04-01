b5a7c4a91180f2b0d58692cc91ef2cbce5172434
V Guardado <auricia.guardado@gmail.com>
2021-03-31 20:07:41 -0700

    prompt user for student basic info

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Student_Management_System
+
+Your firm is implementing its student management prototype in Python.
+It's a command-line program, which the registrar's office will use to add students to the database upon enrollment
---
+++ student-management.py
@@ -0,0 +1,4 @@
+#Prompt user for student's information 
+first_name = input('Please enter the student\'s first name.')
+last_name = input('Please enter the student\'s last name.')
+middle_initial = input('Please enter the student\'s middle initial.')
