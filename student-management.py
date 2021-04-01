e11f6fae423796276d05bd8903587c6418867cf8
V Guardado <auricia.guardado@gmail.com>
2021-03-31 20:25:26 -0700

    simplify the print

--- student-management.py
+++ student-management.py
@@ -1,3 +1,6 @@
+
+student = dict.fromkeys(['first_name','last_name','middle_initial','address','email','phone_number'])
+
 #Prompt user for student's identification information 
 first_name = input('Please enter the student\'s first name: ')
 last_name = input('Please enter the student\'s last name: ')
@@ -13,13 +16,15 @@ print('-' * 18)
 
 #Print everything to the console 
 
-print('The student\'s first name is {0}'.format(first_name))
-print('The student\'s last name is {0}'.format(last_name))
-print('The student\'s middle initial is {0}'.format(middle_initial))
+# print('The student\'s first name is {0}'.format(first_name))
+# print('The student\'s last name is {0}'.format(last_name))
+# print('The student\'s middle initial is {0}'.format(middle_initial))
 
-print('The student\'s address is {0}'.format(address))
-print('The student\'s email is {0}'.format(email))
-print('The student\'s phone number is {0}'.format(phone_number))
+# print('The student\'s address is {0}'.format(address))
+# print('The student\'s email is {0}'.format(email))
+# print('The student\'s phone number is {0}'.format(phone_number))
+
+for key, value in student.items(): print('The student\'s {0}is {1}. '.format(key,value))
 
 #Seperator 
 print('-' * 18)
